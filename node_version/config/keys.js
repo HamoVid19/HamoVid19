5bf9f4af122da2ac1b0d6de31578e2223da39e19
yupher <hamzabouhadjila@gmail.com>
2020-04-20 02:49:14 +0100

    finnished register/login user api

---
+++ node_version/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://yupher:yupher@cluster0-pphwn.azure.mongodb.net/covid19?retryWrites=true&w=majority',
+  secretOrKey: "nigga in jail for rapin a 12 years old white girl",
+}
